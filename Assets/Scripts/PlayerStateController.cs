74bc44a6a72dcb618558f1ae9e4fd654fc62087b
super <juanbass84@gmail.com>
2019-10-22 18:48:26 +0200

    primer Try

---
+++ Assets/Scripts/PlayerStateController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerStateController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start() { }
+    //Definicio dels diferents estats del player  
+    public enum playerStates { idle = 0, left, right, jump, landing, falling, kill, resurrect }
+    //Definicio del delegate playerStateHandler
+    public delegate void playerStateHandler(PlayerStateController.playerStates newState);
+    //Definicio d'event onStateChange i assignacio de onStateChange com a EventHandler
+    public static event playerStateHandler onStateChange;
+    // Aquest metode es crida despres de Update() a cada frame.   
+    void LateUpdate()
+    {
+        // Recollir l'input actual en el Horizontal axis (eix horitzontal)   
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        float jump = Input.GetAxis("Jump");
+        //Tractar segons el valor de l'input recollit    
+        if (horizontal != 0f)
+        {
+            //Hi ha algun moviment: canviar l'estat del protagonista a left o right
+            if (horizontal < 0f) { 
+                if (onStateChange != null) onStateChange(PlayerStateController.playerStates.left);
+            }
+            else { 
+                if (onStateChange != null) onStateChange(PlayerStateController.playerStates.right); 
+            }
+        }
+        else if (jump > 0.0f)
+            {
+                if (onStateChange != null) onStateChange(PlayerStateController.playerStates.jump);
+            }
+        else //No hi ha cap moviment: canviar l'estat del protagonista a idle  
+        { 
+            if (onStateChange != null) onStateChange(PlayerStateController.playerStates.idle); 
+        }
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+}
+
+
+
+
+
+
+
+
